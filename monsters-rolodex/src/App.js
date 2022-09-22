f76ef86916821db15dedf85d3f0024d4ed24bf37
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2022-09-22 15:52:29 +0100

    app optimised

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -32,22 +32,27 @@ class App extends Component {
     el.toLowerCase().includes(query.toLowerCase()));
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();        
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
   render(){
     console.log('render ran');
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    const {monsters, searchField }= this.state;
+    const {onSearchChange }= this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
         <input className='search-box' 
-        type='search' placeholder='seach monsters' onChange={(event) => {
-          const searchField = event.target.value.toLocaleLowerCase();
-          
-          this.setState(() => {
-            return { searchField };
-          });
-        }}     
+        type='search' placeholder='seach monsters' 
+        onChange = {onSearchChange}     
         />
         {
           filteredMonsters.map((monster) => {
